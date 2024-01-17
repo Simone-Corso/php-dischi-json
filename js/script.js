486a0e70f6e35035957b052558a641f24dd70a34
Simone-Corso <scorso26@gmail.com>
2024-01-17 14:45:09 +0100

    inserito il metodo dove prenderà la lista che ho creato nel list.json

--- js/script.js
+++ js/script.js
@@ -7,5 +7,16 @@ createApp({
             albumList:[],
         }
     },
+    methods: {
+        getAlbumList() {
+            axios.get(this.apriUrl).then((response)=> {
+                this.albumList = respose.data;
+                console.log(response.data);
+            })
+        }
+    },
 
-})
+    created(){
+        this.getAlbumList();
+    }
+}).mount('#app')
