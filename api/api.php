a4725337b3011033e267be7b10dcaaff70ec616e
Simone-Corso <scorso26@gmail.com>
2024-01-17 09:24:38 +0100

    npm install vue

--- api/api.php
+++ api/api.php
@@ -1,4 +1,4 @@
-<!-- andrò a inserire la parte dove metterò il url per realizzare la chiamata API !-->
+
 
 
 <?php
@@ -6,6 +6,11 @@ header('Content-Type: application/json');
 
 $albumsJson = file_get_contents('albums.json');
 echo $albumsJson;
+
+$decodedJson = (!is_null($jsonData)) ? json_decode($jsonData, true) : [];
+
+
+echo json_encode(($decodedJson));
 
 
 ?>
