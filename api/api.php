d841470099c76d49b5bdc1ae695b77f6e8e410f1
Simone-Corso <scorso26@gmail.com>
2024-01-17 08:25:28 +0100

    ho tolto l'array di musica perchè andava messo nel albums.json

--- api/api.php
+++ api/api.php
@@ -4,15 +4,6 @@
 <?php
 header('Content-Type: application/json');
 
-$albums = [
-    ['title' => 'Album 1', 'artist' => 'Artista 1'],
-    ['title' => 'Album 1', 'artist' => 'Artista 1'],
-    ['title' => 'Album 1', 'artist' => 'Artista 1'],
-    ['title' => 'Album 1', 'artist' => 'Artista 1'],
-    ['title' => 'Album 1', 'artist' => 'Artista 1'],
-    ['title' => 'Album 1', 'artist' => 'Artista 1'],
-];
-
 $albumsJson = file_get_contents('albums.json');
 echo $albumsJson;
 
